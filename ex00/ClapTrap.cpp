f1b01a8867873fc2dbc271f2a9fa04d2b59c0eb6
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-17 06:26:58 +0000

    Add ClapTrap class implementatifor canonical form

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,31 @@
+
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(const std::string& name)
+    : name(name), hitPoints(10), energyPoints(10), attackDamage(0) {
+    std::cout << "ClapTrap " << name << " has been created!" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+    : name(other.name), hitPoints(other.hitPoints), energyPoints(other.energyPoints), attackDamage(other.attackDamage) {
+    std::cout << "ClapTrap " << name << " has been copied!" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
+    if (this != &other) {
+        name = other.name;
+        hitPoints = other.hitPoints;
+        energyPoints = other.energyPoints;
+        attackDamage = other.attackDamage;
+        std::cout << "ClapTrap " << name << " has been assigned!" << std::endl;
+    }
+    return *this;
+}
+
+ClapTrap::~ClapTrap() {
+    std::cout << "ClapTrap " << name << " has been destroyed!" << std::endl;
+}
+
+
+
