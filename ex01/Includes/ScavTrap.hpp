1ea3e85bcaec69173b74325ee5cd2a5da24bd87c
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-19 21:55:06 +0000

    fix ScavTrap.hpp include path

--- ex01/Includes/ScavTrap.hpp
+++ ex01/Includes/ScavTrap.hpp
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 21:50:53 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/03/19 21:54:20 by ael-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
-#include "../ex00/ClapTrap.hpp"
+#include "ClapTrap.hpp"
 #include <iostream>
 
 class ScavTrap : public ClapTrap {
