29a6e7cc80fc87b62d0c5622b1079a8f4ac252cd
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-19 22:49:41 +0000

    Add FragTrap class header with constructors, destructor, and attack method

---
+++ ex02/Includes/FlagTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FlagTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 22:36:20 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/03/19 22:49:24 by ael-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class FragTrap : public ClapTrap {
+public:
+    // Constructors and destructor
+    FragTrap(const std::string& name);
+    FragTrap(const FragTrap& other);
+    ~FragTrap();
+
+    // Attack method
+    void attack(const std::string& target);
+};
+
+#endif
+
