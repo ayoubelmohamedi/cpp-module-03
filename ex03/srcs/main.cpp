6983f1dd55f2d3e01ed42e0c00d1251fb3be77dc
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-21 00:53:26 +0000

    remove unused func from call

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,27 +6,23 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 21:31:56 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/03/21 00:49:59 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/03/21 00:52:52 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 int main() {
-    // Create a DiamondTrap instance
     DiamondTrap diamond("DiamondBoy");
 
     // Display DiamondTrap's attributes
     diamond.whoAmI();
-    diamond.printStatus();
 
     // Use inherited methods from ScavTrap and FragTrap
     diamond.attack("EnemyBot");    // Should use ScavTrap's attack
     diamond.takeDamage(20);        // Inherited from ClapTrap
     diamond.beRepaired(15);        // Inherited from ClapTrap
 
-    // Check remaining hit points and energy points
-    diamond.printStatus();
 
     return 0;
 }
