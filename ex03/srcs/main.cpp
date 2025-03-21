9665df7cf1b905fa4d686a9d801428abe84f5a80
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-03-21 00:52:07 +0000

    main test for diamondTrap class

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,28 +6,27 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 21:31:56 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/03/20 23:55:13 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/03/21 00:49:59 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
-
+#include "DiamondTrap.hpp"
 
 int main() {
-    
-    FragTrap flag("Flaggy");
+    // Create a DiamondTrap instance
+    DiamondTrap diamond("DiamondBoy");
 
-    flag.attack("Enemy1");
+    // Display DiamondTrap's attributes
+    diamond.whoAmI();
+    diamond.printStatus();
 
-    flag.takeDamage(30);
+    // Use inherited methods from ScavTrap and FragTrap
+    diamond.attack("EnemyBot");    // Should use ScavTrap's attack
+    diamond.takeDamage(20);        // Inherited from ClapTrap
+    diamond.beRepaired(15);        // Inherited from ClapTrap
 
-    flag.beRepaired(20);
-
-    flag.highFivesGuys();
-
-    flag.takeDamage(100); 
-    flag.beRepaired(50);  
-    
+    // Check remaining hit points and energy points
+    diamond.printStatus();
 
     return 0;
 }
